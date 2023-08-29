2434c1f94bbb4c5947cc81afa15fccb4c9307da6
AbrarSaad <abrarsaad3572022@gmail.com>
2023-08-29 12:57:21 -0700

    task 3 Add node at the end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -27,8 +27,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (*head);
 	}
 	lastNode = *head;
-
-	while (lastNode->next != NULL) 
+	while (lastNode->next != NULL)
 	{
 		lastNode = lastNode->next;
 	}
